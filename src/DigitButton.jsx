b2b9c9b744e29b79733ceba06d283aaafc96a8db
Shashank Verma <shashankverma1816@gmail.com>
2024-09-01 13:24:29 +0530

    Updating code via API

--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -2,6 +2,10 @@ import { ACTIONS } from "./App";
 
 export default function DigitButton({ dispatch, digit }) {
   const handleClick = () => {
+    dispatchAction();
+  };
+
+  const dispatchAction = () => {
     try {
       dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
     } catch (error) {
